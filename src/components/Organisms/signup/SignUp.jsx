ef42a507719a415b9280b01184c1b785b308ca89
Moshood-wale <moshood815@gmail.com>
2022-07-17 15:18:27 +0100

    created_signup_page

--- src/components/Organisms/signup/SignUp.jsx
+++ src/components/Organisms/signup/SignUp.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Button, Form, Logo } from "../../Atoms";
+import Field from "../../Molecules";
+import SignUpStyle from "./SignUp.style";
 
 function SignUp() {
+  const handleSubmit = () => {
+    alert("E don submit!");
+  };
   return (
-    <div>SignUp</div>
-  )
+    <>
+      <SignUpStyle>
+        <Logo />
+        <div className="wrapper">
+          <h1>Create your account</h1>
+          <Form>
+            <Field label="Full Name" placeholder="Enter your full name" type="text" />
+            <Field
+              type="image"
+              label="Upload a Photo"
+            />
+            <Field label="Phone Number" placeholder="Enter your phone number" type="number" />
+            <Field label="Joined Since?" placeholder="When did you join?" type="month" />
+            <Button onClick={handleSubmit}>Sign Up</Button>
+          </Form>
+          <p id="bottom">
+            Already have an account? <a href="www">Login</a>
+          </p>
+        </div>
+      </SignUpStyle>
+    </>
+  );
 }
 
 export default SignUp
