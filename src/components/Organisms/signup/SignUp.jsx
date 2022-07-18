dd8641ab8415b0607eb620370e63e3e28705ae63
Moshood-wale <moshood815@gmail.com>
2022-07-18 11:41:04 +0100

    updated signup_page

--- src/components/Organisms/signup/SignUp.jsx
+++ src/components/Organisms/signup/SignUp.jsx
@@ -12,15 +12,16 @@ function SignUp() {
       <SignUpStyle>
         <Logo />
         <div className="wrapper">
-          <h1>Create your account</h1>
+          <h1>Create an account</h1>
           <Form>
             <Field label="Full Name" placeholder="Enter your full name" type="text" />
-            <Field
-              type="image"
-              label="Upload a Photo"
-            />
+            <Field label="Email" placeholder="Enter your email" type="email" />
             <Field label="Phone Number" placeholder="Enter your phone number" type="number" />
-            <Field label="Joined Since?" placeholder="When did you join?" type="month" />
+            <Field
+              type="password"
+              placeholder="Enter your password"
+              label="Password"
+            />
             <Button onClick={handleSubmit}>Sign Up</Button>
           </Form>
           <p id="bottom">
