36ea40150a24506733c23b662b553d3ff7d345bd
Deyems <adeyemisola11@gmail.com>
2022-08-07 00:47:34 +0100

    reformatted document and added new routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import { MyDeliveries } from "./components";
-import { SignUp, Login, Homepage, Verification, ForgotPassword, CreateNewPassword, ProfilePage} from "./pages";
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import { MyDeliveries } from './components'
+import {
+  SignUp,
+  Login,
+  Homepage,
+  Verification,
+  ForgotPassword,
+  CreateNewPassword,
+  ProfilePage,
+  UpdatePasswordPage,
+  EditProfilePage
+} from './pages'
 
 function App() {
   return (
@@ -11,12 +21,14 @@ function App() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/new-password" element={<CreateNewPassword />} />    
+      <Route path="/change-password" element={<UpdatePasswordPage />} />
+      <Route path="/new-password" element={<CreateNewPassword />} />
       <Route path="/verification" element={<Verification />} />
-      <Route path="/my_deliveries" element={ <MyDeliveries/>} />
-      <Route path="/profile" element={ <ProfilePage/>} />
+      <Route path="/my_deliveries" element={<MyDeliveries />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/edit-profile" element={<EditProfilePage />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
