2e8e7d41b7a9bfa06a467c6b5f0196a8255578dd
Adeyemi sola <45840438+Deyems@users.noreply.github.com>
2022-08-18 11:38:18 +0100

    Revert "console error fixed"

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect} from "react";
 import { AuthContext } from "../context/AuthProvider";
 import { NavBar } from "../components/Molecules";
 function Profile() {
