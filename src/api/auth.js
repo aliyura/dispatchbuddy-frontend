b55a9514bd6dfa8c8c89887e6624d3df1d667937
Deyems <adeyemisola11@gmail.com>
2022-08-22 12:35:38 +0100

    update profile call

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,5 @@
 import axios from "./axios";
+const { REACT_APP_AUTH_TOKEN } = process.env;
 
 // import queryString from "querystring"
 export const login = async ({ grant_type, email, password }) => {
@@ -160,3 +161,79 @@ export const reset = async (username = "net.rabiualiyu@gmail.com", { password })
   }
 };
 
+export const updateProfile = async(username, country, city, gender, dob) => {
+  try {
+//     const response = await axios.post(
+//       "api/user/update",
+//       JSON.stringify({
+//         name: username,
+//         country,
+//         city,
+//         gender,
+//         dateOfBirth: dob,
+//       }),
+//       {
+//         headers: { "Content-type": "application/json",
+//         Authorization: `${JSON.parse(localStorage.getItem(REACT_APP_AUTH_TOKEN))?.token}`
+//       },
+// }
+//     );
+    // console.log(response, 'from updateProfile Call backend ...');
+    let response = {
+      data: {
+      "message": "Request Successful",
+      "success": true,
+      "payload": {
+          "id": "62da61914a0ad40de7754454",
+          "name": "Rabiu Deyems",
+          "uuid": "64048f36099",
+          "email": "net.rabiualiyu@gmail.com",
+          "password": "$2a$10$1urNhtYII0LixhJR0OVr1ur77eE7iB8gbPqAEAfpfOF0B19KzmEXa",
+          "phoneNumber": "08064160204",
+          "gender": "Male",
+          "city": "Lagos",
+          "country": "Kenya",
+          "dp": "dp64048f36099.jpg",
+          "dateOfBirth": "02/04/1994",
+          "thirdPartyToken": null,
+          "isEnabled": true,
+          "authProvider": "EMAIL",
+          "accountType": "DISPATCHER",
+          "status": "AC",
+          "role": "USER",
+          "coveredLocations": [
+              "Somehwere",
+              "Itedo",
+              "Ikeja",
+              "Ajah",
+              "Ikorodu",
+              "Iyana Ikpaja",
+              "Ikotun",
+              "Iyana Ipaja",
+              "Ikoyi",
+              "Idimu",
+              "Victoria Island",
+              "Lekki",
+              "[LOCALITY, POLITICAL, GEOCODE]",
+              "[ADMINISTRATIVE_AREA_LEVEL_3, POLITICAL, GEOCODE]",
+              "Ajah/Sangotedo"
+          ],
+          "createdDate": "2022-07-22T08:36:33.094+00:00",
+          "updatedDate": "2022-08-19T15:28:20.966+00:00",
+          "lastLoginDate": "2022-07-22T08:36:33.094+00:00",
+          "lastModifiedDate": "2022-08-19T15:28:20.966+00:00"
+      }
+    }
+  }
+    return {
+      data: response,
+      error: null,
+    };
+  } catch (err) {
+    console.log(err, 'at update route...');
+    return {
+      data: null,
+      error: err,
+    };
+  }
+}
