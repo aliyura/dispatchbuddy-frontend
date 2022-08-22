6ddb5e7dfc5a5194029c461294bb6daa074f8b0e
Deyems <adeyemisola11@gmail.com>
2022-08-22 12:34:22 +0100

    profile edit update

--- src/components/Organisms/editProfile/EditProfile.jsx
+++ src/components/Organisms/editProfile/EditProfile.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import { Button, Form, Logo } from '../../Atoms'
-import { Field } from '../../Molecules'
-import EditProfileStyle from './editProfile.style'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState, useCallback } from 'react';
+import { Button, Form, Logo } from '../../Atoms';
+import { Field } from '../../Molecules';
+import EditProfileStyle from './editProfile.style';
+import { useNavigate } from 'react-router-dom';
+import axios from "../../../api/axios";
+import countryData from './CountryData.json';
+
+import { AuthContext } from "../../../context/AuthProvider";
+import { updateProfile } from "../../../api";
+import swal from "sweetalert";
+import { apiPost } from '../../../auth/apiHelper';
+
+const {REACT_APP_AUTH_TOKEN} = process.env;
 
 function EditProfile() {
-  const handleSubmit = () => {
-    alert('Submitted!')
+  const [username, setUsername] = useState('');
+  const [country, setCountry] = useState('');
+  const [city, setCity] = useState('');
+  const [gender, setGender] = useState('');
+  const [dob, setDate] = useState('');
+
+  console.log(JSON.parse(localStorage.getItem(REACT_APP_AUTH_TOKEN))?.token, 'collected token');
+
+  const handleSubmit = async (e) => {
+    // alert('Submitted!');
+    e.preventDefault();
+    console.log('values passed',  username, country, city, gender, dob);
+    const {data, error} = await updateProfile(username, country, city, gender, dob);
+    
+    // console.log(response, 'from backend');
+    console.log(data, 'from recieved backend');
+    // return;
+
+    if (data?.data?.success) {
+      swal({
+        text: "Profile update Successful",
+        icon: "success",
+        button: false,
+        timer: 3000,
+      });
+      // setCode("");
+      navigate("/profile");
+    } else if (!data?.data?.success) {
+      swal("Oops", data?.data?.message, "error", {
+        button: false,
+        timer: 3000,
+      });
+    } else {
+      swal("Oops", error, "error", {
+        button: false,
+        timer: 3000,
+      });
+    }
   }
 
   const navigate = useNavigate()
@@ -15,6 +60,31 @@ function EditProfile() {
 //   "city":"Lagos",
 //   "gender":"Male",
 //   "dateOfBirth":"02/04/1994"
+  const handleChangeName = (e) => {
+    setUsername(e.target.value);
+  }
+  const handleChangeCountry = (e) => {
+    setCountry(e.target.value);
+  }
+  const handleChangeCity = (e) => {
+    setCity(e.target.value);
+  }
+
+  const handleChangeGender = (e) => {
+    setGender(e.target.value);
+  }
+  const handleChangeDate = (e) => {
+    setDate(e.target.value);
+  }
+
+  // useEffect(() => {
+  //   // handleChangeName();
+  //   // handleChangeCountry();
+  //   // handleChangeCity();
+  //   // handleChangeDate();
+  //   // handleSubmit()
+  // }, []);
+
   return (
     <>
       <EditProfileStyle>
@@ -32,28 +102,38 @@ function EditProfile() {
               type="text"
               placeholder="Enter your name"
               label="Username"
+              value={username}
+              handleChange={handleChangeName}
             />
-            <Field
-              type="text"
-              placeholder="Enter your country"
-              label="Country"
-            />
+            <div className="select_grp">
+              <label htmlFor="country">Country</label>
+              <select name="country" id="country" className="classic" onChange={handleChangeCountry}>
+                {countryData['Africa'].map((item, idx) => <option key={idx} value={item}>{item}</option>)}
+              </select>
+            </div>
             <Field
               type="text"
               placeholder="Enter your City"
               label="City"
+              value={city}
+              handleChange={handleChangeCity}
             />
+            <div className="select_grp">
+              <label htmlFor="city">Gender</label>
+              <select name='gender' id='gender' className="classic" onChange={handleChangeGender}>
+                <option key={1} value={""}>Gender</option>
+                <option key={2} value={"Male"}>Male</option>
+                <option key={3} value={"Female"}>Female</option>
+              </select>
+            </div>
             <Field
-              type="text"
-              placeholder="Enter your Gender"
-              label="Gender"
-            />
-            <Field
-              type="text"
+              type="date"
               placeholder="Enter your Date of Birth"
               label="DOB"
+              handleChange={handleChangeDate}
+              value={dob}
             />
-            <Button className="update_btn" onClick={handleSubmit}>
+            <Button className="update_btn" onClick={(e) => handleSubmit(e)}>
               Update Profile
             </Button>
           </Form>
--- src/components/Organisms/editProfile/editProfile.style.js
+++ src/components/Organisms/editProfile/editProfile.style.js
@@ -41,6 +41,65 @@ const EditProfileStyle = styled.div`
      .update_btn{
         cursor: pointer;
      }
+     .select_grp{
+      //  border: 2px solid red;
+      padding-bottom: 0.7rem 0;
+       select {
+         width: 100%;
+        /* styling */
+        background-color: transparent;
+        // border: thin solid blue;
+        // border-radius: 4px;
+        display: inline-block;
+        font: inherit;
+        line-height: 1.5em;
+        padding: 0.5em 3.5em 0.5em 1em;
+        outline: none;
+        // border: none;
+        border: 1px solid #d9d9d9;
+        
+        /* reset */
+        margin: 0;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+      }
+       .classic{
+        background-image:
+        linear-gradient(45deg, transparent 50%, blue 50%),
+        linear-gradient(135deg, blue 50%, transparent 50%),
+        linear-gradient(to right, skyblue, skyblue);
+      background-position:
+        calc(100% - 20px) calc(1em + 2px),
+        calc(100% - 15px) calc(1em + 2px),
+        100% 0;
+      background-size:
+        5px 5px,
+        5px 5px,
+        2.5em 2.5em;
+      background-repeat: no-repeat;
+       }
+       .classic:focus {
+        background-image:
+          linear-gradient(45deg, white 50%, transparent 50%),
+          linear-gradient(135deg, transparent 50%, white 50%),
+          linear-gradient(to right, gray, gray);
+        background-position:
+          calc(100% - 15px) 1em,
+          calc(100% - 20px) 1em,
+          100% 0;
+        background-size:
+          5px 5px,
+          5px 5px,
+          2.5em 2.5em;
+        background-repeat: no-repeat;
+        // border-color: grey;
+        outline: 0;
+      }
+      
+     }
     @media (max-width: 500px) {
       width: 80%;
       h1 {
